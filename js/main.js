9440aaf89f17726bfdf8e9e4946571d1121356a7
asandelarvine <asandelarvine@gmail.com>
2022-09-01 01:44:32 +0300

    updated styling on team and testimonials

--- js/main.js
+++ js/main.js
@@ -1,169 +1,114 @@
-
-    // <!-- script for form validation -->
+(function ($) {
+    "use strict";
     
-         (function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      var form = document.getElementById('needs-validation');
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
         }
-        form.classList.add('was-validated');
-      }, false);
-    }, false);
-  })();
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
     
-
-
-      
-
-    //   <!-- script for testimonials slide-effect -->
-      
-          $(document).ready(function() {
-          $('#testimony1').click(function() {
-                  $('.testimony1').toggle("slide");
-          });
-      });
-
-          $(document).ready(function() {
-              $('#testimony2').click(function() {
-                      $('.testimony2').toggle("slide");
-              });
-          });
-
-          $(document).ready(function() {
-          $('#testimony3').click(function() {
-                  $('.testimony3').toggle("slide");
-          });
-      });
-      
-
- 
-
-
-    // other law site
-
-        // Sticky Navbar
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 90) {
-                $('.nav-bar').addClass('nav-sticky');
-                $('.carousel, .page-header').css("margin-top", "73px");
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.nav-bar').addClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "73px");
+        } else {
+            $('.nav-bar').removeClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "0");
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
             } else {
-                $('.nav-bar').removeClass('nav-sticky');
-                $('.carousel, .page-header').css("margin-top", "0");
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
             }
-        });
-        
-        
-    
-    (function ($) {
-        "use strict";
-        
-        // Dropdown on mouse hover
-        $(document).ready(function () {
-            function toggleNavbarMethod() {
-                if ($(window).width() > 992) {
-                    $('.navbar .dropdown').on('mouseover', function () {
-                        $('.dropdown-toggle', this).trigger('click');
-                    }).on('mouseout', function () {
-                        $('.dropdown-toggle', this).trigger('click').blur();
-                    });
-                } else {
-                    $('.navbar .dropdown').off('mouseover').off('mouseout');
-                }
-            }
-            toggleNavbarMethod();
-            $(window).resize(toggleNavbarMethod);
-        });
-        
-        
-        // Back to top button
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 100) {
-                $('.back-to-top').fadeIn('slow');
-            } else {
-                $('.back-to-top').fadeOut('slow');
-            }
-        });
-        $('.back-to-top').click(function () {
-            $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-            return false;
-        });
-    
-    
-        // Date and time picker
-        $('#date').datetimepicker({
-            format: 'L'
-        });
-        $('#time').datetimepicker({
-            format: 'LT'
-        });
-    
-    
-        // Service carousel
-        $(".service-carousel").owlCarousel({
-            autoplay: true,
-            smartSpeed: 1500,
-            margin: 30,
-            dots: false,
-            loop: true,
-            nav : true,
-            navText : [
-                '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-                '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-            ],
-            responsive: {
-                0:{
-                    items:1
-                },
-                576:{
-                    items:1
-                },
-                768:{
-                    items:2
-                },
-                992:{
-                    items:3
-                }
-            }
-        });
-    
-    
-        
-    
-        // Testimonials carousel
-        $(".testimonial-carousel").owlCarousel({
-            center: true,
-            autoplay: true,
-            smartSpeed: 1000,
-            margin: 30,
-            dots: true,
-            loop: true,
-            responsive: {
-                0:{
-                    items:1
-                },
-                576:{
-                    items:1
-                },
-                768:{
-                    items:2
-                },
-                992:{
-                    items:3
-                }
-            }
-        });
-        
-    })(jQuery);
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
 
 
-    // script 4 serv
+    'use strict'
+    var testim = document.getElementById("testim"),
+        testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+        testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+        testimleftArrow = document.getElementById("left-arrow"),
+        testimRightArrow = document.getElementById("right-arrow"),
+        testimSpeed = 4500,
+        currentSlide = 0,
+        currentActive = 0,
+        testimTimer
+        ;
+    // coding with nick
+    window.onload = function () {
+    
+        // Testim Script
+        function playSlide(slide) {
+            for (var k = 0; k < testimDots.length; k++) {
+                testimContent[k].classList.remove("active");
+                testimContent[k].classList.remove("inactive");
+                testimDots[k].classList.remove("active");
+            }
+            if (slide < 0) {
+                slide = currentSlide = testimContent.length - 1;
+            }
+            if (slide > testimContent.length - 1) {
+                slide = currentSlide = 0;
+            }
+            if (currentActive != currentSlide) {
+                testimContent[currentActive].classList.add("inactive");
+            }
+            testimContent[slide].classList.add("active");
+            testimDots[slide].classList.add("active");
+    
+            currentActive = currentSlide;
+    
+            clearTimeout(testimTimer);
+            testimTimer = setTimeout(function () {
+                playSlide(currentSlide += 1);
+            }, testimSpeed)
+        }
+    // coding with nick
+        testimleftArrow.addEventListener("click", function () {
+            playSlide(currentSlide -= 1);
+        })
+        testimRightArrow.addEventListener("click", function () {
+            playSlide(currentSlide += 1);
+        })
+    
+        for (var l = 0; l < testimDots.length; l++) {
+            testimDots[l].addEventListener("click", function () {
+                playSlide(currentSlide = testimDots.indexOf(this));
+            })
+        }
+        playSlide(currentSlide);
+    
+    }
+    
+    
+    
+  
+    
+})(jQuery);
 
-      
 
-    
-    
-    
+
+// coding with nick
+
