6f2ac91aa41af868cb0a6dbd8b3b6b8a03ae49e0
Larvine <asandelarvine@gmail.com>
2024-04-25 23:29:06 +0300

    footer changed in all pages

--- js/main.js
+++ js/main.js
@@ -1,112 +1,91 @@
-
-(function ($) {
-    "use strict";
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-    
-    
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 90) {
-            $('.nav-bar').addClass('nav-sticky');
-            $('.carousel, .page-header').css("margin-top", "73px");
-        } else {
-            $('.nav-bar').removeClass('nav-sticky');
-            $('.carousel, .page-header').css("margin-top", "0");
-        }
-    });
-    
-    
-    // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-
-
-    'use strict'
-    var testim = document.getElementById("testim"),
-        testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
-        testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
-        testimleftArrow = document.getElementById("left-arrow"),
-        testimRightArrow = document.getElementById("right-arrow"),
-        testimSpeed = 4500,
-        currentSlide = 0,
-        currentActive = 0,
-        testimTimer
-        ;
-    
-    window.onload = function () {
-    
-        // Testim Script
-        function playSlide(slide) {
-            for (var k = 0; k < testimDots.length; k++) {
-                testimContent[k].classList.remove("active");
-                testimContent[k].classList.remove("inactive");
-                testimDots[k].classList.remove("active");
-            }
-            if (slide < 0) {
-                slide = currentSlide = testimContent.length - 1;
-            }
-            if (slide > testimContent.length - 1) {
-                slide = currentSlide = 0;
-            }
-            if (currentActive != currentSlide) {
-                testimContent[currentActive].classList.add("inactive");
-            }
-            testimContent[slide].classList.add("active");
-            testimDots[slide].classList.add("active");
-    
-            currentActive = currentSlide;
-    
-            clearTimeout(testimTimer);
-            testimTimer = setTimeout(function () {
-                playSlide(currentSlide += 1);
-            }, testimSpeed)
-        }
-    
-        testimleftArrow.addEventListener("click", function () {
-            playSlide(currentSlide -= 1);
-        })
-        testimRightArrow.addEventListener("click", function () {
-            playSlide(currentSlide += 1);
-        })
-    
-        for (var l = 0; l < testimDots.length; l++) {
-            testimDots[l].addEventListener("click", function () {
-                playSlide(currentSlide = testimDots.indexOf(this));
-            })
-        }
-        playSlide(currentSlide);
-    
-    }
-    
-
-    
+(function($) {
+	"use strict";
+	// Back to top button
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 200) {
+			$('.back-to-top').fadeIn('slow');
+		} else {
+			$('.back-to-top').fadeOut('slow');
+		}
+	});
+	$('.back-to-top').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1500, 'easeInOutExpo');
+		return false;
+	});
+	// Sticky Navbar
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 90) {
+			$('.nav-bar').addClass('nav-sticky');
+			$('.carousel, .page-header').css("margin-top", "73px");
+		} else {
+			$('.nav-bar').removeClass('nav-sticky');
+			$('.carousel, .page-header').css("margin-top", "0");
+		}
+	});
+	// Dropdown on mouse hover
+	$(document).ready(function() {
+		function toggleNavbarMethod() {
+			if ($(window).width() > 992) {
+				$('.navbar .dropdown').on('mouseover', function() {
+					$('.dropdown-toggle', this).trigger('click');
+				}).on('mouseout', function() {
+					$('.dropdown-toggle', this).trigger('click').blur();
+				});
+			} else {
+				$('.navbar .dropdown').off('mouseover').off('mouseout');
+			}
+		}
+		toggleNavbarMethod();
+		$(window).resize(toggleNavbarMethod);
+	});
+	'use strict'
+	var testim = document.getElementById("testim"),
+		testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+		testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+		testimleftArrow = document.getElementById("left-arrow"),
+		testimRightArrow = document.getElementById("right-arrow"),
+		testimSpeed = 4500,
+		currentSlide = 0,
+		currentActive = 0,
+		testimTimer;
+	window.onload = function() {
+		// Testim Script
+		function playSlide(slide) {
+			for (var k = 0; k < testimDots.length; k++) {
+				testimContent[k].classList.remove("active");
+				testimContent[k].classList.remove("inactive");
+				testimDots[k].classList.remove("active");
+			}
+			if (slide < 0) {
+				slide = currentSlide = testimContent.length - 1;
+			}
+			if (slide > testimContent.length - 1) {
+				slide = currentSlide = 0;
+			}
+			if (currentActive != currentSlide) {
+				testimContent[currentActive].classList.add("inactive");
+			}
+			testimContent[slide].classList.add("active");
+			testimDots[slide].classList.add("active");
+			currentActive = currentSlide;
+			clearTimeout(testimTimer);
+			testimTimer = setTimeout(function() {
+				playSlide(currentSlide += 1);
+			}, testimSpeed)
+		}
+		testimleftArrow.addEventListener("click", function() {
+			playSlide(currentSlide -= 1);
+		})
+		testimRightArrow.addEventListener("click", function() {
+			playSlide(currentSlide += 1);
+		})
+		for (var l = 0; l < testimDots.length; l++) {
+			testimDots[l].addEventListener("click", function() {
+				playSlide(currentSlide = testimDots.indexOf(this));
+			})
+		}
+		playSlide(currentSlide);
+	}
 })(jQuery);
-
-  
-  
-
